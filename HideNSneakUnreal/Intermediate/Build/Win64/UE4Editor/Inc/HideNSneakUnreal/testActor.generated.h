960eb6a30c394a73a31527cffbfe69b45398d259
Oskar Johansson <oskar_9393@hotmail.com>
2020-10-16 12:42:34 +0200

    testing stuff

---
+++ HideNSneakUnreal/Intermediate/Build/Win64/UE4Editor/Inc/HideNSneakUnreal/testActor.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef HIDENSNEAKUNREAL_testActor_generated_h
+#error "testActor.generated.h already included, missing '#pragma once' in testActor.h"
+#endif
+#define HIDENSNEAKUNREAL_testActor_generated_h
+
+#define HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_SPARSE_DATA
+#define HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_RPC_WRAPPERS
+#define HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAtestActor(); \
+	friend struct Z_Construct_UClass_AtestActor_Statics; \
+public: \
+	DECLARE_CLASS(AtestActor, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/HideNSneakUnreal"), NO_API) \
+	DECLARE_SERIALIZER(AtestActor)
+
+
+#define HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAtestActor(); \
+	friend struct Z_Construct_UClass_AtestActor_Statics; \
+public: \
+	DECLARE_CLASS(AtestActor, AActor, COMPILED_IN_FLAGS(0 | CLASS_Config), CASTCLASS_None, TEXT("/Script/HideNSneakUnreal"), NO_API) \
+	DECLARE_SERIALIZER(AtestActor)
+
+
+#define HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AtestActor(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AtestActor) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AtestActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AtestActor); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AtestActor(AtestActor&&); \
+	NO_API AtestActor(const AtestActor&); \
+public:
+
+
+#define HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AtestActor(AtestActor&&); \
+	NO_API AtestActor(const AtestActor&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AtestActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AtestActor); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AtestActor)
+
+
+#define HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_PRIVATE_PROPERTY_OFFSET
+#define HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_9_PROLOG
+#define HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_SPARSE_DATA \
+	HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_RPC_WRAPPERS \
+	HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_INCLASS \
+	HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_SPARSE_DATA \
+	HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_INCLASS_NO_PURE_DECLS \
+	HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> HIDENSNEAKUNREAL_API UClass* StaticClass<class AtestActor>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID HideNSneakUnreal_Source_HideNSneakUnreal_testActor_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
