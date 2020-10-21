ada8ea59819f52efd23316bd60f66103686206c9
abdisuleiman7 <abdifatah.suleiman7@gmail.com>
2020-10-22 00:27:08 +0200

    Powerup overlap

---
+++ HideNSneakUnreal/Source/HideNSneakUnreal/CPP_Powerup.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CPP_Powerup.h"
+#include <Components/BoxComponent.h>
+#include <Engine/Engine.h>
+
+// Sets default values
+ACPP_Powerup::ACPP_Powerup()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	CollisionBox->SetBoxExtent(FVector(50.f, 50.f, 50.f));
+	CollisionBox->SetCollisionProfileName("Trigger");
+	RootComponent = CollisionBox;
+
+	CollisionBox->OnComponentBeginOverlap.AddDynamic(this,&ACPP_Powerup::OnOverlapBegin);
+	
+	
+	
+	
+
+
+	
+	//Setting the mesh
+
+	UStaticMeshComponent* Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("My PowerUp"));
+	Mesh->SetupAttachment(RootComponent);
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> Powerup(TEXT("/Game/StarterContent/Shapes/Shape_QuadPyramid.Shape_QuadPyramid"));
+	if (Powerup.Succeeded()) {
+		Mesh->SetStaticMesh(Powerup.Object);
+		Mesh->SetRelativeLocation(FVector(0.0f, 0.0f, 0.0f));
+		Mesh->SetWorldScale3D(FVector(1.f));
+	}
+
+
+
+
+	
+	
+	
+
+}
+
+// Called when the game starts or when spawned
+void ACPP_Powerup::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPP_Powerup::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ACPP_Powerup::OnOverlapBegin(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+	GEngine->AddOnScreenDebugMessage(-1, 1.f, FColor::Yellow, "Overlap Begin called");
+}
+
---
+++ HideNSneakUnreal/Source/HideNSneakUnreal/CPP_Powerup.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include <Components/StaticMeshComponent.h>
+#include <GameFrameWork/Actor.h>
+#include <Components/BoxComponent.h>
+
+#include "CPP_Powerup.generated.h"
+
+
+UCLASS()
+class HIDENSNEAKUNREAL_API ACPP_Powerup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACPP_Powerup();
+
+	
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+		class UBoxComponent* CollisionBox;
+
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlapComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	
+	//UFUNCTION()
+	//	void OnOverlapBegin(class UPrimitiveComponent* OverlapComponent, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+};
