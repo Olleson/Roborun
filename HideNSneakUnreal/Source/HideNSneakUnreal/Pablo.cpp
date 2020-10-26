688123b33dba683653ff65559e424aa9575540cf
WINDSV\osjo8879 <asda>
2020-10-26 14:47:59 +0100

    Pulling Pablo into me

---
+++ HideNSneakUnreal/Source/HideNSneakUnreal/Pablo.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Pablo.h"
+
+// Sets default values
+APablo::APablo()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APablo::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APablo::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
