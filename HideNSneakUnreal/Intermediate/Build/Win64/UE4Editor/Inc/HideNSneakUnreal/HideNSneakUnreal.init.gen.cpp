f82254909f5b73922fd0f139f61ee218ee9d4707
Aulin93 <66680398+Aulin93@users.noreply.github.com>
2020-10-27 15:05:45 +0100

    Miracles happen?

--- HideNSneakUnreal/Intermediate/Build/Win64/UE4Editor/Inc/HideNSneakUnreal/HideNSneakUnreal.init.gen.cpp
+++ HideNSneakUnreal/Intermediate/Build/Win64/UE4Editor/Inc/HideNSneakUnreal/HideNSneakUnreal.init.gen.cpp
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
 // Copyright Epic Games, Inc. All Rights Reserved.
 /*===========================================================================
 	Generated code exported from UnrealHeaderTool.
@@ -28,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeHideNSneakUnreal_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x61435B83,
-				0xCE095D9D,
+				0x4AC3BA0A,
+				0xD4C9A2C9,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
@@ -40,67 +33,3 @@ PRAGMA_ENABLE_DEPRECATION_WARNINGS
 #ifdef _MSC_VER
 #pragma warning (pop)
 #endif
-=======
->>>>>>> 6448b747bd9a6770408d24df2f5a33bedb1e4913
->>>>>>> 7c5ce777cd00b6ee6046f129026ffbde988bab21
->>>>>>> 9758f3933b07d87b05f15562af536d305f16c2b5
-// Copyright Epic Games, Inc. All Rights Reserved.
-/*===========================================================================
-	Generated code exported from UnrealHeaderTool.
-	DO NOT modify this manually! Edit the corresponding .h files instead!
-===========================================================================*/
-
-#include "UObject/GeneratedCppIncludes.h"
-#ifdef _MSC_VER
-#pragma warning (push)
-#pragma warning (disable : 4883)
-#endif
-PRAGMA_DISABLE_DEPRECATION_WARNINGS
-void EmptyLinkFunctionForGeneratedCodeHideNSneakUnreal_init() {}
-	UPackage* Z_Construct_UPackage__Script_HideNSneakUnreal()
-	{
-		static UPackage* ReturnPackage = nullptr;
-		if (!ReturnPackage)
-		{
-			static const UE4CodeGen_Private::FPackageParams PackageParams = {
-				"/Script/HideNSneakUnreal",
-				nullptr,
-				0,
-				PKG_CompiledIn | 0x00000000,
-<<<<<<< HEAD
-				0x61435B83,
-				0xCE095D9D,
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-				0x61435B83,
-				0xCE095D9D,
-=======
-				0xCD937450,
-				0x316336A1,
->>>>>>> 6448b747bd9a6770408d24df2f5a33bedb1e4913
-=======
-				0x250A3E9F,
-				0x42F47882,
->>>>>>> 42f0643de269f3f43e79bd4fa738a91aec78cd07
->>>>>>> 9758f3933b07d87b05f15562af536d305f16c2b5
-				METADATA_PARAMS(nullptr, 0)
-			};
-			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
-		}
-		return ReturnPackage;
-	}
-PRAGMA_ENABLE_DEPRECATION_WARNINGS
-#ifdef _MSC_VER
-#pragma warning (pop)
-#endif
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> Aulin
->>>>>>> 6448b747bd9a6770408d24df2f5a33bedb1e4913
->>>>>>> 7c5ce777cd00b6ee6046f129026ffbde988bab21
->>>>>>> 9758f3933b07d87b05f15562af536d305f16c2b5
--- HideNSneakUnreal/Intermediate/Build/Win64/UE4Editor/Inc/HideNSneakUnreal/HideNSneakUnrealClasses.h
+++ HideNSneakUnreal/Intermediate/Build/Win64/UE4Editor/Inc/HideNSneakUnreal/HideNSneakUnrealClasses.h
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-=======
-<<<<<<< HEAD
->>>>>>> 7c5ce777cd00b6ee6046f129026ffbde988bab21
 // Copyright Epic Games, Inc. All Rights Reserved.
 /*===========================================================================
 	Generated code exported from UnrealHeaderTool.
@@ -16,30 +8,3 @@
 
 
 
-=======
-<<<<<<< HEAD
-=======
->>>>>>> 6448b747bd9a6770408d24df2f5a33bedb1e4913
->>>>>>> 7c5ce777cd00b6ee6046f129026ffbde988bab21
->>>>>>> 9758f3933b07d87b05f15562af536d305f16c2b5
-// Copyright Epic Games, Inc. All Rights Reserved.
-/*===========================================================================
-	Generated code exported from UnrealHeaderTool.
-	DO NOT modify this manually! Edit the corresponding .h files instead!
-===========================================================================*/
-
-#pragma once
-
-
-
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 42f0643de269f3f43e79bd4fa738a91aec78cd07
-=======
-<<<<<<< HEAD
-=======
->>>>>>> Aulin
->>>>>>> 6448b747bd9a6770408d24df2f5a33bedb1e4913
->>>>>>> 7c5ce777cd00b6ee6046f129026ffbde988bab21
->>>>>>> 9758f3933b07d87b05f15562af536d305f16c2b5
