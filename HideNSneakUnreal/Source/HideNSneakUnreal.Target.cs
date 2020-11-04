ff68577efcdc2c77d26e1edd938f2011ff6e0274
Oskar Johansson <Oskar_9393@hotmail.com>
2020-11-04 13:43:54 +0100

    Fixing merge issues in main (it now starts)

--- HideNSneakUnreal/Source/HideNSneakUnreal.Target.cs
+++ HideNSneakUnreal/Source/HideNSneakUnreal.Target.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
@@ -14,20 +13,3 @@ public class HideNSneakUnrealTarget : TargetRules
 		ExtraModuleNames.AddRange( new string[] { "HideNSneakUnreal" } );
 	}
 }
-=======
-// Fill out your copyright notice in the Description page of Project Settings.
-
-using UnrealBuildTool;
-using System.Collections.Generic;
-
-public class HideNSneakUnrealTarget : TargetRules
-{
-	public HideNSneakUnrealTarget(TargetInfo Target) : base(Target)
-	{
-		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-
-		ExtraModuleNames.AddRange( new string[] { "HideNSneakUnreal" } );
-	}
-}
->>>>>>> Aulin
--- HideNSneakUnreal/Source/HideNSneakUnreal/TagMechanic.cpp
+++ HideNSneakUnreal/Source/HideNSneakUnreal/TagMechanic.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 //Author: Olle Frid
 
 #include "TagMechanic.h"
@@ -52,59 +51,4 @@ void ATagMechanic::OnCompHit(UPrimitiveComponent* HitComp, AActor* OtherActor, U
 void ATagMechanic::TurnIntoSeeker() {
 	GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, FString::Printf(TEXT("Someone has been turned into a Seeker!")));
 	targetTagMechanic->isSeeker = true;
-=======
-//Author: Olle Frid
-
-#include "TagMechanic.h"
-#include "Components/BoxComponent.h"
-#include "Kismet/GameplayStatics.h"
-#include "GameplayTagsModule.h"
-#include "GameplayTagsSettings.h"
-#include "GameplayTags.h"
-#include "GameplayTagsManager.h"
-#include "GameFramework/Actor.h"
-
-// Sets default values
-ATagMechanic::ATagMechanic() {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
-
-	MyComp = CreateDefaultSubobject<UBoxComponent>(TEXT("CapComp"));
-	MyComp->SetSimulatePhysics(true);
-	MyComp->SetNotifyRigidBodyCollision(true);
-	MyComp->BodyInstance.SetCollisionProfileName("BlockAllDynamic");
-	MyComp->OnComponentHit.AddDynamic(this, &ATagMechanic::OnCompHit);
-
-	RootComponent = MyComp;
-}
-
-// Called when the game starts or when spawned
-void ATagMechanic::BeginPlay() {
-	Super::BeginPlay();
-
-}
-
-// Called every frame
-void ATagMechanic::Tick(float DeltaTime) {
-	Super::Tick(DeltaTime);
-}
-
-//Checks if collision has occurred
-//HitComp is this ATagMechanic
-//OtherActor is whatever actor this objects bumps into
-void ATagMechanic::OnCompHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit){
-
-	if (OtherActor->IsA(ATagMechanic::StaticClass()) && OtherActor != this && !Cast<ATagMechanic>(OtherActor)->isSeeker && Cast<ATagMechanic>(this)->isSeeker) {
-		targetActor = OtherActor;
-		targetTagMechanic = Cast<ATagMechanic>(targetActor);
-		TurnIntoSeeker();
-	}
-}
-
-//Plays when someone gets turned into a seeker
-//Basically just flips a boolean to true
-void ATagMechanic::TurnIntoSeeker() {
-	GEngine->AddOnScreenDebugMessage(-1, 1.0f, FColor::Red, FString::Printf(TEXT("Someone has been turned into a Seeker!")));
-	targetTagMechanic->isSeeker = true;
->>>>>>> Aulin
 }
