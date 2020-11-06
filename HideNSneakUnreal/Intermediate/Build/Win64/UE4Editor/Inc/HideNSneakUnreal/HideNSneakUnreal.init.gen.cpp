e545ddd41be5ee5fcff05a8e8d2bd3968bad983b
Oskar Johansson <Oskar_9393@hotmail.com>
2020-11-06 13:27:13 +0100

    fuckit

--- HideNSneakUnreal/Intermediate/Build/Win64/UE4Editor/Inc/HideNSneakUnreal/HideNSneakUnreal.init.gen.cpp
+++ HideNSneakUnreal/Intermediate/Build/Win64/UE4Editor/Inc/HideNSneakUnreal/HideNSneakUnreal.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeHideNSneakUnreal_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x10E5E0FC,
-				0x3D351AB4,
+				0x3E77CE54,
+				0xCC889D7D,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
