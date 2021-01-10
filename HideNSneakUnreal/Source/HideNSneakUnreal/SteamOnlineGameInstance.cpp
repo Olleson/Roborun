54097220e1c9c82637438b11c87e6df59860de6a
ollefrid <olllefrid@gmail.com>
2021-01-10 12:19:49 +0100

    pee

--- HideNSneakUnreal/Source/HideNSneakUnreal/SteamOnlineGameInstance.cpp
+++ HideNSneakUnreal/Source/HideNSneakUnreal/SteamOnlineGameInstance.cpp
@@ -122,6 +122,7 @@ void USteamOnlineGameInstance::CreateServer(FString ServerName, FString HostName
 	SessionSettings.bShouldAdvertise = true;
 	SessionSettings.bUsesPresence = true;
 	SessionSettings.NumPublicConnections = MaxPlayerCount;
+
 	SessionSettings.Set(FName("SERVER_NAME_KEY"), ServerName, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
 	SessionSettings.Set(FName("SERVER_HOSTNAME_KEY"), HostName, EOnlineDataAdvertisementType::ViaOnlineServiceAndPing);
 
